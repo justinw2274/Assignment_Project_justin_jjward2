01b3255fa70ec73bc4a9d3752c734b780cf28d5c
justinw2274 <129928848+justinw2274@users.noreply.github.com>
2025-09-22 19:44:47 -0500

    Added view funtions

--- paper_trader/views.py
+++ paper_trader/views.py
@@ -1,3 +1,28 @@
+from django.http import HttpResponse
+from django.template import loader
 from django.shortcuts import render
+from .models import Trade
 
-# Create your views here.
+
+def trade_list_http(request):
+
+    all_trades = Trade.objects.all()
+
+    template = loader.get_template("papertrader/trade_list.html")
+
+    context = {
+        "trades": all_trades,
+    }
+
+    return HttpResponse(template.render(context, request))
+
+
+def trade_list_render(request):
+
+    all_trades = Trade.objects.all()
+
+    context = {
+        "trades": all_trades,
+    }
+
+    return render(request, "papertrader/trade_list.html", context)
