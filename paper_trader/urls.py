d50b511f450828e25d2d244e07e72338f0db4a7c
justinw2274 <129928848+justinw2274@users.noreply.github.com>
2025-10-27 18:00:48 -0500

    Added new url functionality to redirect the default url to the home page

--- paper_trader/urls.py
+++ paper_trader/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from django.views.generic.base import RedirectView
 
 app_name = 'paper_trader'
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='paper_trader:strategy_list_generic', permanent=True)),
+
     path('trades/http/', views.trade_list_http, name='trade_list_http'),
 
     path('trades/render/', views.trade_list_render, name='trade_list_render'),
