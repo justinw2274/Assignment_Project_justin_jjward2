405fef973bee247b85ea02352c8c8d581375e3f2
justinw2274 <129928848+justinw2274@users.noreply.github.com>
2025-11-12 16:54:54 -0600

    General website debugging for signup and logout functionality

--- backtesting_platform/urls.py
+++ backtesting_platform/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from paper_trader import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup/', views.signup, name='signup'),
     path('', include('paper_trader.urls')),
 ]
